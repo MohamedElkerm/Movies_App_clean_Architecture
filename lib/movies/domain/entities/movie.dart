ba39358dadbc9484d85d3de33bdd05bb7a57c128
Mohamed Elkerm <mohamedelkerm584@gmail.com>
2023-01-31 03:40:09 +0200

    init files

---
+++ lib/movies/domain/entities/movie.dart
@@ -0,0 +1,11 @@
+class Movie {
+  final int id;
+  final String title;
+  final String backdrop_path;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.backdrop_path,
+  });
+}
